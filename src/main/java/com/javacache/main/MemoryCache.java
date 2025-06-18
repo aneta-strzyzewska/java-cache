e70b09dcb833a2790161729614f77c8dea47b79d
Aneta <aneta.strzyzewska@outlook.com>
2025-06-18 13:57:45 +0200

    implement cache singleton

--- src/main/java/com/javacache/main/MemoryCache.java
+++ src/main/java/com/javacache/main/MemoryCache.java
@@ -1,4 +1,19 @@
 package com.javacache.main;
 
+import java.util.concurrent.ConcurrentHashMap;
+
 public class MemoryCache {
+    private static ConcurrentHashMap<String, String> INSTANCE;
+
+    private MemoryCache() { }
+
+    public static ConcurrentHashMap<String, String> getInstance() {
+        if (INSTANCE == null) {
+            INSTANCE = new ConcurrentHashMap<>();
+
+            return INSTANCE;
+        }
+        return INSTANCE;
+    }
+
 }
