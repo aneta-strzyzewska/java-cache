58e984d61b2ab38659e1216e2e32359d74cccdde
Aneta <aneta.strzyzewska@outlook.com>
2025-06-21 04:37:25 +0200

    initialize server

--- src/main/java/com/javacache/main/Main.java
+++ src/main/java/com/javacache/main/Main.java
@@ -1,8 +1,15 @@
 package com.javacache.main;
+
+import org.apache.cxf.jaxrs.JAXRSServerFactoryBean;
+import org.apache.cxf.jaxrs.lifecycle.SingletonResourceProvider;
+
 public class Main {
 
 	public static void main(String[] args) {
-		Server server = new Server();
+		JAXRSServerFactoryBean serverFactory = new JAXRSServerFactoryBean();
+		serverFactory.setResourceProvider(new SingletonResourceProvider(new RandomNameController()));
+		serverFactory.setAddress("http://localhost:8080/");
+		serverFactory.create();
 	}
 
 }
