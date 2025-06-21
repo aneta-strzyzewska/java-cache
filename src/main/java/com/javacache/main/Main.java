92c5be698c44b94314294594e789e9258049eaa5
Aneta <aneta.strzyzewska@outlook.com>
2025-06-21 16:36:19 +0200

    restructure main a bit

--- src/main/java/com/javacache/main/Main.java
+++ src/main/java/com/javacache/main/Main.java
@@ -1,15 +1,22 @@
 package com.javacache.main;
 
+import org.apache.cxf.endpoint.Server;
 import org.apache.cxf.jaxrs.JAXRSServerFactoryBean;
 import org.apache.cxf.jaxrs.lifecycle.SingletonResourceProvider;
 
 public class Main {
 
+	private final static String ADDRESS = "http://localhost:8080/";
+
 	public static void main(String[] args) {
 		JAXRSServerFactoryBean serverFactory = new JAXRSServerFactoryBean();
 		serverFactory.setResourceProvider(new SingletonResourceProvider(new RandomNameController()));
-		serverFactory.setAddress("http://localhost:8080/");
-		serverFactory.create();
+		serverFactory.setAddress(ADDRESS);
+		Server server = serverFactory.create();
+
+		if(server.isStarted()) {
+			System.out.println("Server listening on " + ADDRESS);
+		}
 	}
 
 }
