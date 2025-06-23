886aa433d2b44777bef2eca010f210404427e54c
Aneta <aneta.strzyzewska@outlook.com>
2025-06-23 09:07:54 +0200

    add logger

--- src/main/java/com/javacache/app/Main.java
+++ src/main/java/com/javacache/app/Main.java
@@ -3,19 +3,23 @@ package com.javacache.app;
 import org.apache.cxf.endpoint.Server;
 import org.apache.cxf.jaxrs.JAXRSServerFactoryBean;
 import org.apache.cxf.jaxrs.lifecycle.SingletonResourceProvider;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Main {
 
 	private final static String ADDRESS = "http://localhost:8080/";
 
 	public static void main(String[] args) {
+		Logger log = LoggerFactory.getLogger(Main.class);
+
 		JAXRSServerFactoryBean serverFactory = new JAXRSServerFactoryBean();
 		serverFactory.setResourceProvider(new SingletonResourceProvider(new RandomNameController()));
 		serverFactory.setAddress(ADDRESS);
 		Server server = serverFactory.create();
 
 		if(server.isStarted()) {
-			System.out.println("Server listening on " + ADDRESS);
+			log.info("Server listening on " + ADDRESS);
 		}
 	}
 
